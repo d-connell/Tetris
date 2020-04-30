a2afa26853ff0a4e5651c40819cd0999aa46a7e8
d-connell <lapetitegecko@gmail.com>
2020-04-30 16:30:57 +1000

    minor revisions

--- src/main/java/tetris/Launcher.java
+++ src/main/java/tetris/Launcher.java
@@ -1,13 +1,11 @@
 package tetris;
 
 import tetris.game.DefaultTetris;
-import tetris.game.Game;
 
 public class Launcher {
 
     public static void main(String[] args) {
-        Game game = new DefaultTetris();
-        game.start();
+        new DefaultTetris().start();
     }
 
 }
